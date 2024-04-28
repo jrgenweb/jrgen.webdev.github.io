c64c8335f7927333334b25a14e923e66ed58a632
Laszlo Nemeth <nemeth.rzr@gmail.com>
2024-04-28 18:24:52 +0200

    added more projects, and load more btn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,122 +1,104 @@
-
 /** Animation On Scroll */
 AOS.init();
 
+const projectsContentEl = document.querySelector(".projects__content"); //projects container
+const navToggleEl = document.querySelector(".nav-toggle"); // navigation button
+const navEl = document.querySelector("nav .wrapper"); //top navigation
 
-const projectsContentEl = document.querySelector('.projects__content'); //projects container
-const navToggleEl = document.querySelector('.nav-toggle'); // navigation button
-const navEl = document.querySelector('nav .wrapper');//top navigation
+navToggleEl.addEventListener("click", handleNavigation);
 
-
-
-navToggleEl.addEventListener('click', handleNavigation);
-
+let projects = [];
 
 /**
  * swicth light and dark theme
  * detect the defalt scheme
- * 
+ *
  */
-const btnThemeSwitch = document.getElementById('themeMode');
-const body = document.querySelector('body');
-
-
+const btnThemeSwitch = document.getElementById("themeMode");
+const body = document.querySelector("body");
 
 /** */
-btnThemeSwitch.addEventListener('change', () => {
-    console.log('Change');
+btnThemeSwitch.addEventListener("change", () => {
+  console.log("Change");
 
-    if (btnThemeSwitch.checked) {
+  if (btnThemeSwitch.checked) {
+    //set to dark
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
 
-        //set to dark
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
+    console.log("dark");
+  } else {
+    //set to light
+    body.classList.add("light-theme");
+    body.classList.remove("dark-theme");
 
-
-        console.log('dark');
-
-
-    } else {
-        //set to light
-        body.classList.add('light-theme');
-        body.classList.remove('dark-theme');
-
-        console.log('light');
-    }
-})
+    console.log("light");
+  }
+});
 /**
- * 
- * @param {*} scheme 
- * add class to body 
- * 
- * 
+ *
+ * @param {*} scheme
+ * add class to body
+ *
+ *
  */
 function setColorScheme(scheme) {
-    switch (scheme) {
-        case 'dark':
-            body.classList.remove('light-theme');
-            body.classList.add('dark-theme');
-            btnThemeSwitch.checked = true;
-            console.log('dark');
-            //Dark
-            break;
-        case 'light':
-            body.classList.add('light-theme');
-            body.classList.remove('dark-theme');
-            btnThemeSwitch.checked = false;
-            console.log('light');
-            // Light
-            break;
-        default:
-            // Default
-            console.log('light');
-            break;
-    }
+  switch (scheme) {
+    case "dark":
+      body.classList.remove("light-theme");
+      body.classList.add("dark-theme");
+      btnThemeSwitch.checked = true;
+      console.log("dark");
+      //Dark
+      break;
+    case "light":
+      body.classList.add("light-theme");
+      body.classList.remove("dark-theme");
+      btnThemeSwitch.checked = false;
+      console.log("light");
+      // Light
+      break;
+    default:
+      // Default
+      console.log("light");
+      break;
+  }
 }
 
-function setSwicth(scheme) {
-
-}
+function setSwicth(scheme) {}
 function getPreferredColorScheme() {
-    if (window.matchMedia) {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            return 'dark';
-        } else {
-            return 'light';
-        }
+  if (window.matchMedia) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
     }
-    return 'light';
+  }
+  return "light";
 }
 
 function updateColorScheme() {
-    setColorScheme(getPreferredColorScheme());
+  setColorScheme(getPreferredColorScheme());
 }
 /** if change the color scheme */
 if (window.matchMedia) {
-    var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    colorSchemeQuery.addEventListener('change', updateColorScheme);
+  var colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  colorSchemeQuery.addEventListener("change", updateColorScheme);
 }
 
 updateColorScheme();
 
-
-
 //add or remove class from nav element
 function handleNavigation() {
-    navEl.classList.toggle('active');
-    console.log('working')
+  navEl.classList.toggle("active");
 }
-
-
-
 
 //load projects from data.json
 async function loadProjects() {
-    const response = await fetch('public/data.json');
-    const projects = await response.json();
-    renderCard(projects);
+  const response = await fetch("public/data.json");
+  projects = await response.json();
+  renderCard(projects);
 }
-
 
 //For create a card Element
 /* <div class="card">
@@ -139,73 +121,122 @@ async function loadProjects() {
 
 </div> */
 
-
-/** render cards */
+/**
+ * render cards
+ *
+ * first time render the first 6 card
+ *
+ *
+ */
 function renderCard(data) {
-    data.forEach(element => {
-        projectsContentEl.appendChild(
-            createCard(element));
-    });
+  for (let j = 0; j < 6; j++) {
+    projectsContentEl.appendChild(createCard(data[j]));
+  }
+
+  const loadMoreBtn = document.createElement("a");
+  loadMoreBtn.href = "#";
+  loadMoreBtn.innerText = "Load More";
+  loadMoreBtn.classList.add("btn-load-more");
+  loadMoreBtn.addEventListener("click", loadMoreCard);
+  projectsContentEl.parentNode.appendChild(loadMoreBtn);
+  /* data.forEach((element) => {
+    projectsContentEl.appendChild(createCard(element));
+  }); */
 }
+
+/**
+ *
+ * when click load more button,
+ * then load another 6 card, and add a load more button, while the all cards rendered
+ *
+ */
+function loadMoreCard(event) {
+  event.preventDefault();
+
+  /**
+   * first determine the starting position
+   */
+
+  let lastCards = false;
+  const cards = document.querySelectorAll(".projects__content .card");
+  const loadMoreBtn = document.querySelector(".btn-load-more");
+
+  const startingPosition = cards.length;
+  const endPosition =
+    projects.length - startingPosition >= 6
+      ? startingPosition + 6
+      : (() => {
+          lastCards = true;
+          return startingPosition + (projects.length - startingPosition);
+        })();
+  //remove load more btn
+
+  console.log("end", endPosition);
+
+  let helper = projects.length - startingPosition;
+  console.log(helper, cards.length);
+
+  for (let i = startingPosition; i < endPosition; i++) {
+    //projectsContentEl.insertBefore(createCard(projects[i]), loadMoreBtn);
+    projectsContentEl.appendChild(createCard(projects[i]));
+  }
+  if (lastCards) loadMoreBtn.remove();
+}
+
 /**create card element */
 function createCard(item) {
+  const cardEl = document.createElement("div");
+  cardEl.classList.add("card");
+  cardEl.dataset.aos = "zoom-in";
+  const cardImageEl = document.createElement("div");
+  cardImageEl.classList.add("card__image");
+  const imgEl = document.createElement("img");
+  if (item.category != "FEM") {
+    imgEl.src = "dist/img/projects/" + item.img_url;
+  } else {
+    imgEl.src = item.img_url;
+  }
+  imgEl.alt = item.title + " screenshot";
 
-    const cardEl = document.createElement('div');
-    cardEl.classList.add('card');
-    cardEl.dataset.aos = "zoom-in";
-    const cardImageEl = document.createElement('div');
-    cardImageEl.classList.add('card__image');
-    const imgEl = document.createElement('img');
-    if (item.category != 'FEM') {
-        imgEl.src = 'dist/img/projects/' + item.img_url;
-    } else {
-        imgEl.src = item.img_url;
+  cardImageEl.appendChild(imgEl);
+  const cardContentEl = document.createElement("div");
+  cardContentEl.classList.add("card__content");
+  const cardTitleEl = document.createElement("h3");
+  cardTitleEl.innerText = item.title;
+  const cardDescriptionEl = document.createElement("p");
+  cardDescriptionEl.innerText = item.description;
 
-    }
-    imgEl.alt = item.title + ' screenshot';
+  cardContentEl.appendChild(cardTitleEl);
+  cardContentEl.appendChild(cardDescriptionEl);
 
-    cardImageEl.appendChild(imgEl);
-    const cardContentEl = document.createElement('div');
-    cardContentEl.classList.add('card__content');
-    const cardTitleEl = document.createElement('h3');
-    cardTitleEl.innerText = item.title;
-    const cardDescriptionEl = document.createElement('p');
-    cardDescriptionEl.innerText = item.description;
+  const cardTagsEl = document.createElement("div");
+  cardTagsEl.classList.add("card__tags");
+  cardTagsEl.innerText = item.tags.toString();
 
-    cardContentEl.appendChild(cardTitleEl);
-    cardContentEl.appendChild(cardDescriptionEl);
+  const cardFooterEl = document.createElement("div");
+  cardFooterEl.classList.add("card__footer");
 
+  const cardLinkView = document.createElement("a");
+  cardLinkView.classList.add("btn-link", "link--preview");
+  cardLinkView.href = item.urls.live;
+  cardLinkView.target = "_blank";
+  cardLinkView.innerText = "Liew Preview";
 
-    const cardTagsEl = document.createElement('div');
-    cardTagsEl.classList.add('card__tags');
-    cardTagsEl.innerText = item.tags.toString();
+  const cardLinkCode = document.createElement("a");
+  cardLinkCode.classList.add("btn-link", "link--code");
+  cardLinkCode.href = item.urls.code;
+  cardLinkCode.target = "_blank";
+  cardLinkCode.innerText = "View Code";
 
-    const cardFooterEl = document.createElement('div');
-    cardFooterEl.classList.add('card__footer');
+  cardFooterEl.appendChild(cardLinkView);
+  cardFooterEl.appendChild(cardLinkCode);
 
-    const cardLinkView = document.createElement('a');
-    cardLinkView.classList.add('btn-link', 'link--preview')
-    cardLinkView.href = item.urls.live;
-    cardLinkView.target = "_blank";
-    cardLinkView.innerText = "Liew Preview"
+  cardEl.appendChild(cardImageEl);
+  cardEl.appendChild(cardContentEl);
+  cardEl.appendChild(cardTagsEl);
+  cardEl.appendChild(cardFooterEl);
 
-    const cardLinkCode = document.createElement('a');
-    cardLinkCode.classList.add('btn-link', 'link--code')
-    cardLinkCode.href = item.urls.code;
-    cardLinkCode.target = "_blank";
-    cardLinkCode.innerText = "View Code"
-
-    cardFooterEl.appendChild(cardLinkView);
-    cardFooterEl.appendChild(cardLinkCode);
-
-    cardEl.appendChild(cardImageEl);
-    cardEl.appendChild(cardContentEl);
-    cardEl.appendChild(cardTagsEl);
-    cardEl.appendChild(cardFooterEl);
-
-    return cardEl;
-
+  return cardEl;
 }
 
-
-loadProjects()
+loadProjects();
